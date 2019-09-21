d7827674ecd77fe19ff2f4a9be638405a330124e
tarunraghav226 <tarun.raghav226@gmail.com>
2019-09-21 21:16:11 +0530

    Completed Stage 1 of hyperskills.

--- Tic-Tac-Toe with AI/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe with AI/task/src/tictactoe/Main.java
@@ -2,6 +2,11 @@ package tictactoe;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                System.out.print("X");
+            }
+            System.out.println();
+        }
     }
 }
