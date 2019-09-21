9dd047158d2d969960397e448c849a79804d1191
tarunraghav226 <tarun.raghav226@gmail.com>
2019-09-21 21:34:38 +0530

    Completed Stage 2 of hyperskills.

--- Tic-Tac-Toe with AI/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe with AI/task/src/tictactoe/Main.java
@@ -1,12 +1,20 @@
 package tictactoe;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        for(int i=0;i<3;i++){
-            for(int j=0;j<3;j++){
-                System.out.print("X");
-            }
-            System.out.println();
-        }
+    public static String displaGame(String pattern) {
+       return "---------"+"\n"
+               +"| "+pattern.charAt(1)+" "+pattern.charAt(2)+" "+pattern.charAt(3)+" |"+"\n"
+               +"| "+pattern.charAt(4)+" "+pattern.charAt(5)+" "+pattern.charAt(6)+" |"+"\n"
+               +"| "+pattern.charAt(7)+" "+pattern.charAt(8)+" "+pattern.charAt(9)+" |"+"\n"
+               +"---------";
     }
+
+    public static void main(String[] args) {
+            Scanner scan =new Scanner(System.in);
+            String pattern=scan.nextLine();
+            System.out.println(displaGame(pattern));
+            scan.close();
+        }
+
 }
